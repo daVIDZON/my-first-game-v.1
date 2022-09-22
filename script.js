882afc2970597e5c7b19f396a9faf84390520241
David <david.bogomolov2@gmail.com>
2022-09-22 14:13:17 +0300

    added start menu and attack

--- script.js
+++ script.js
@@ -2,96 +2,29 @@ const Player = document.querySelector('.persone');
 const Attack = document.querySelector('.attack');
 const ZoneArea = document.querySelector('.main-area');
 const ZoneSpwnEnemies = document.querySelector('.ZoneSpwnEnemies');
+const playerZone = document.querySelector('.PlayerZone');
 const WholeArea = document.querySelector('.area');
 const removeEnemyArea = document.querySelector('.enemy-animation');
-
+const startGame = document.querySelector('.game-start');
+const startMenuOfgame = document.querySelector('.start-menu');
 
 //timer
 let timerNum = document.querySelector('.timer-style');
 const clock = document.querySelector('.clock');
 
 
-let clone = Attack.cloneNode(true);
-
-let posX = 50;
-let posY = 50;
-let attackPos = 0;
-
-window.addEventListener('keydown', move);
-// window.addEventListener('keydown', attack);
-
-function move(event) {
-
-    if (event.code == 'KeyW' && Player.style.top >= '21%') {
-        posY -= 3;
-    } else if (event.code == 'KeyA' && Player.style.left >= '17%') {
-        posX -= 3;
-    } else if (event.code == 'KeyS' && Player.style.top <= '73%') {
-        posY += 3;
-    } else if (event.code == 'KeyD' && Player.style.left <= '78%') {
-        posX += 3;
-    }
-
-    Player.style.cssText = `
-     top:${posY}%;
-     left:${posX}%;
-    transition-duration: 0.5s;
-    `;
-}
-
-// function attack(event) {
-//     setTimeout(()=>{
-//         if(event.code == 'Space'){
-//             attackPos+=500;
-//           Area.appendChild(clone);
-//         }
-//         else{
-//          console.log('trabbles');
-//         }
-
-//         Attack.style.cssText = `
-//         bottom:${attackPos}px;
-//        `;
-
-//     },1500);
-// }
-
-function enemies() {
-    setInterval(() => {
-        let newEnemy = document.createElement('div');
-        newEnemy.classList.add('enemy');
-        const space = Math.floor(Math.random() * 1000);
-        if (space < 330) {
-            newEnemy.remove();
-        } else {
-            newEnemy.style.left = space + "px";
-            setTimeout(() => {
-                newEnemy.classList.add('enemy-animation');
-
-                newEnemy.addEventListener("animationend",()=>{
-                    newEnemy.remove();
-                });
-                ZoneSpwnEnemies.appendChild(newEnemy);
-            }, 50);
-        }
-    }, 2000);
+let posX = 700;
+let posY = 75;
 
 
 
-}
-enemies();
+function allActions(){
+    window.addEventListener('keydown', move);
 
 
-
-
-
-
-
-
-
-//timer
-
-function timeout() {
+ //timer
+    
+ function timeout() {
     let sec = 0;
     let minut = 0;
 
@@ -146,4 +79,109 @@ function timeout() {
         }
     }
 }
+
 timeout();
+
+    function move(event) {
+        // if (event.code == 'KeyW' && Player.style.top >= '21%') {
+        //     posY -= 3;
+        // } 
+        if (event.code == 'KeyA' && posX >= 365) {
+            posX -= 19;
+            // } else if (event.code == 'KeyS' && Player.style.top <= '73%') {
+            //     posY += 3;
+        } else if (event.code == 'KeyD' && posX <= 1030) {
+            posX += 19;
+        }
+    
+        Player.style.cssText = `
+         top:${posY}%;
+         left:${posX}px;
+        transition-duration: 0.3s;
+        `;
+    }
+    
+    
+    
+    function enemies() {
+        let newEnemy = document.createElement('div');
+        newEnemy.classList.add('enemy');
+        const space = Math.floor(Math.random() * 1000);
+        if (space < 330) {
+            newEnemy.remove();
+        } else {
+            newEnemy.style.left = space + "px";
+    
+            setTimeout(() => {
+                newEnemy.classList.add('enemy-animation');
+    
+                newEnemy.addEventListener("animationend", () => {
+                    newEnemy.remove();
+                });
+                ZoneSpwnEnemies.appendChild(newEnemy);
+            }, 50);
+        }
+    
+    
+        function attack(event) {
+            let attackCircle = document.createElement('div');
+            attackCircle.classList.add('attack');
+            attackCircle.style.left += 3;
+            if (event.code == 'Space') {
+                attackCircle.classList.add('attack-animation');
+                attackCircle.style.left = Player.style.left;
+                playerZone.appendChild(attackCircle);
+            }
+    
+            attackCircle.addEventListener("animationend", () => {
+                attackCircle.remove();
+            });
+    
+    
+    
+    
+            //убрал поведение браузера на скрол при спейсе
+            var SPACE_KEYCODE = 32;
+            document.onkeydown = function (e) {
+                var keycode = e.keyCode || e.charCode,
+                    body = document.body;
+    
+                if (keycode != SPACE_KEYCODE)
+                    return;
+    
+                e.preventDefault();
+                body.style.overflow = body.style.overflow == 'hidden' ? 'auto' : 'hidden';
+            };
+    
+            if (newEnemy.style.left == attackCircle.style.left
+            ) {
+                console.log('yes');
+            }
+    
+    
+        }
+        window.addEventListener('keydown', attack);
+    }
+    setInterval(() => {
+        enemies();
+    }, 2000);
+    
+    
+}
+allActions();
+startMenuOfgame.style.display="none";
+
+//Menu Start
+
+// function startMenu(){
+//     ZoneArea.style.display="none";
+
+//     startGame.addEventListener("click", () => {
+//         ZoneArea.style.display="flex";
+//         startMenuOfgame.style.display="none";
+//         allActions();
+//     });
+    
+
+// }
+// startMenu();
